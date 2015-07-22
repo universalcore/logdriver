44e4131f6659edff04c285f87b7720e38fabd4cf
Simon de Haan <simon@praekeltfoundation.org>
2015-07-22 09:22:14 +0200

    use gimme to manage go versions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd "${WORKSPACE}/${REPO}"
 export GOPATH=`pwd`
+
+mkdir ./bin
 mkdir ${BUILDDIR}/${REPO}
+
+curl -sL -o ./bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x ./bin/gimme
+
+eval "$(gimme 1.4)"
+
 go get ./... -v
 go build -o ${BUILDDIR}/${REPO}/logdriver -v
