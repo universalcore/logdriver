e53f822262d20f62a32ad67dbb02b854dfdff852
Simon de Haan <simon@praekeltfoundation.org>
2015-07-22 10:29:07 +0200

    fix substitution

--- scripts/install_golang.sh
+++ scripts/install_golang.sh
@@ -1,5 +1,5 @@
 function install_golang {
-    GOLANG_TAG=${$1:"go1.4.1"}
+    GOLANG_TAG=${$1:-"go1.4.1"}
     INSTALL_DIR=$HOME/go-builds/$GOLANG_TAG
     mkdir -p $INSTALL_DIR
     git clone https://go.googlesource.com/go $INSTALL_DIR
