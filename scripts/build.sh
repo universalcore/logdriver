2bfe29e4c83b0ad3a7add2ba26919a274a9816e7
Simon de Haan <simon@praekeltfoundation.org>
2015-07-22 10:37:13 +0200

    do not rebuild if already exists

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,14 @@ cd "${WORKSPACE}/${REPO}"
 
 GOLANG_TAG="go1.4.1"
 INSTALL_DIR=./go-builds/$GOLANG_TAG
-mkdir -p $INSTALL_DIR
-git clone https://go.googlesource.com/go $INSTALL_DIR
-cd $INSTALL_DIR
-git checkout $GOLANG_TAG
-cd src
-bash ./all.bash
+if [ ! -d $INSTALL_DIR ]; then
+    mkdir -p $INSTALL_DIR
+    git clone https://go.googlesource.com/go $INSTALL_DIR
+    cd $INSTALL_DIR
+    git checkout $GOLANG_TAG
+    cd src
+    bash ./all.bash
+fi
 
 mkdir ${BUILDDIR}/${REPO}
 
