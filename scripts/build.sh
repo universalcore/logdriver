1f877712d2942b7403a3e4f6254242cf7fac05f2
Simon de Haan <simon@praekeltfoundation.org>
2015-07-22 10:30:55 +0200

    back into 1 file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 cd "${WORKSPACE}/${REPO}"
 
-source ./scripts/install_golang.sh
-install_golang go1.4.1
+
+GOLANG_TAG="go1.4.1"
+INSTALL_DIR=$HOME/go-builds/$GOLANG_TAG
+mkdir -p $INSTALL_DIR
+git clone https://go.googlesource.com/go $INSTALL_DIR
+cd $INSTALL_DIR
+git checkout $GOLANG_TAG
+cd src
+bash ./all.bash
 
 mkdir ${BUILDDIR}/${REPO}
 
